61015e0e1560bbc47ca3c0914bb5ec232ce9f753
Waans14 <Afwan.sutdrajat@gmail.com>
2022-01-15 12:58:56 +0700

    MyKalkulatorApp

---
+++ app/src/main/java/com/afonetech/mykalkulator/MainActivity.java
@@ -0,0 +1,67 @@
+package com.afonetech.mykalkulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etNilaia, etNilaib;
+    private TextView tvhsl_hitung;
+    private Button btn_hitung;
+    private RadioButton rbTambah, rbKurang, rbKali, rbBagi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNilaia = findViewById(R.id.etNilaia);
+        etNilaib = findViewById(R.id.etNilaib);
+        tvhsl_hitung= findViewById(R.id.tvhsl_hitung);
+        btn_hitung= findViewById(R.id.btn_hitung);
+        rbTambah= findViewById(R.id.rbTambah);
+        rbKurang= findViewById(R.id.rbKurang);
+        rbKali= findViewById(R.id.rbKali);
+        rbBagi= findViewById(R.id.rbBagi);
+
+        btn_hitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(etNilaia.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Nilai A nya Masih Kosong!!",Toast.LENGTH_SHORT).show();
+                }else if(etNilaib.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Nilai B nya Masih Kosong!!",Toast.LENGTH_SHORT).show();
+                }else{
+                    double NilaiA = Double.valueOf(etNilaia.getText().toString());
+                    double NilaiB = Double.valueOf(etNilaib.getText().toString());
+                    double Hasil;
+
+                    if(rbTambah.isChecked()){
+                        Hasil = NilaiA + NilaiB;
+                        tvhsl_hitung.setText(String.valueOf(Hasil));
+                    }else if(rbKurang.isChecked()){
+                        Hasil = NilaiA - NilaiB;
+                        tvhsl_hitung.setText(String.valueOf(Hasil));
+                    }else if(rbKali.isChecked()){
+                        Hasil = NilaiA * NilaiB;
+                        tvhsl_hitung.setText(String.valueOf(Hasil));
+                    }else if(rbBagi.isChecked()){
+                        Hasil = NilaiA / NilaiB;
+                        tvhsl_hitung.setText(String.valueOf(Hasil));
+                    }else{
+                        Toast.makeText(MainActivity.this, "Silahkan Pilih Operatornya Terlebih Dahulu!!",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+    }
+}
